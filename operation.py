3029e4352b1280185fd7e9841cbab2de16a54dd4
Shaikh Mohammad Muazzam Abdul Mubin <muazzamshaikh333@gmail.com>
2026-07-15 23:06:22 +0530

    Create operation.py to perform multiple arithmetic operators in python

---
+++ operation.py
@@ -0,0 +1,20 @@
+n =15
+m = 7
+
+ans1 = n + m
+print("Addition od n and m is:",ans1)
+
+ans2= n - m
+print("Sub of n amd m is:",ans2)
+
+ans3 = n * m 
+print("Multiplication of n amd m is:",ans3)
+
+ans4 = n/m
+print("Diviion of n amd m is:",ans4)
+
+ans5 = n//m
+print("Answer of this operaton is",ans5)
+
+ans6 = n**m
+print("Exponential of n and m is:",ans6)
